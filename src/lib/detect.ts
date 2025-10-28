bbd62cad4720c117805fc034d2d1a3fbebe029c7
Elverum <yoyochinese0001@gmail.com>
2025-10-29 02:31:06 +0900

    fix: match updated format of user-agent coming from umami_flutter

--- src/lib/detect.ts
+++ src/lib/detect.ts
@@ -150,7 +150,7 @@ export async function getClientInfo(request: Request, payload: Record<string, an
   const country = location?.country;
   const region = location?.region;
   const city = location?.city;
-  const browser = userAgent.includes('Tompedia') ? 'Tompedia' : browserName(userAgent);
+  const browser = userAgent.includes('jp.tompedia') ? 'Tompedia' : browserName(userAgent);
   const os = detectOS(userAgent) as string;
   const device = getDevice(payload?.screen, os);
 
