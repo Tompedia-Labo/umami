2f2b0c552b7fb429ca392fc05fbc61aca263fd1e
Elverum <yoyochinese0001@gmail.com>
2025-12-11 01:06:03 +0900

    chore: test setting sslmode search params to require if missing from url string

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -15,6 +15,11 @@ if (process.env.SKIP_DB_CHECK) {
 
 const url = new URL(process.env.DATABASE_URL);
 
+if (!url.searchParams.has('sslmode')) {
+  console.log('enforcing sslmode=require in database connection');
+  url.searchParams.set('sslmode', 'require');
+}
+
 const adapter = new PrismaPg(
   { connectionString: url.toString() },
   { schema: url.searchParams.get('schema') },
