f500144313d48aa71fad7af32a0e73b0c89a1da4
Elverum <yoyochinese0001@gmail.com>
2025-12-12 03:38:02 +0900

    chore: match upstream

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -15,13 +15,8 @@ if (process.env.SKIP_DB_CHECK) {
 
 const url = new URL(process.env.DATABASE_URL);
 
-if (!url.searchParams.has('sslmode')) {
-  console.log('enforcing sslmode=require in database connection');
-  url.searchParams.set('sslmode', 'require');
-}
-
 const adapter = new PrismaPg(
-  { connectionString: url.toString(), ssl: { rejectUnauthorized: false } },
+  { connectionString: url.toString() },
   { schema: url.searchParams.get('schema') },
 );
 
